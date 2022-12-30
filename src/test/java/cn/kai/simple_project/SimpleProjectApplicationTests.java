c11dc639b17ab8cae114bbcd136526a751bae27a
陈凯 <chenkai@asto-inc.com>
2022-12-30 17:16:05 +0800

    init project

---
+++ src/test/java/cn/kai/simple_project/SimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.kai.simple_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
